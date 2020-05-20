443669365f32a205642f5eac9a6f601fec7513b3
Shafiqa Iqbal <33774527+ShafiqaIqbal@users.noreply.github.com>
2020-05-20 19:53:44 +0500

    Update refresh-quicksight-spice-dataset-lambda-function.py

--- refresh-quicksight-spice-dataset-lambda-function.py
+++ refresh-quicksight-spice-dataset-lambda-function.py
@@ -4,8 +4,10 @@ import calendar
 
 
 def lambda_handler(event, context):
-    client = boto3.client('quicksight',  region_name="us-east-1")
-    res = client.list_data_sets(AwsAccountId='134867112207')
+    client = boto3.client('quicksight',  region_name="region")
+    res = client.list_data_sets(AwsAccountId='account-id')
+
+    # filter out your datasets using a prefix. All my datasets have chicago_crimes as their prefix
     datasets_ids = [summary["DataSetId"] for summary in res["DataSetSummaries"] if "chicago_crimes" in summary["Name"]]
     ingestion_ids = []
 
@@ -13,7 +15,7 @@ def lambda_handler(event, context):
         try:
             ingestion_id = str(calendar.timegm(time.gmtime()))
             client.create_ingestion(DataSetId=dataset_id, IngestionId=ingestion_id,
-                                                 AwsAccountId='134867112207')
+                                                 AwsAccountId='account-id')
             ingestion_ids.append(ingestion_id)
         except Exception as e:
             print(e)
@@ -23,7 +25,7 @@ def lambda_handler(event, context):
         while True:
             response = client.describe_ingestion(DataSetId=dataset_id,
                                                  IngestionId=ingestion_id,
-                                                 AwsAccountId='134867112207')
+                                                 AwsAccountId='account-id')
             if response['Ingestion']['IngestionStatus'] in ('INITIALIZED', 'QUEUED', 'RUNNING'):
                 time.sleep(5)     #change sleep time according to your dataset size
             elif response['Ingestion']['IngestionStatus'] == 'COMPLETED':
